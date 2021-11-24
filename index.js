d4df0362b72755ad2263e6cc179c6b9992a18f2f
FlyingndCoding <94818620+FlyingndCoding@users.noreply.github.com>
2021-11-24 14:58:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
  //      Spam Bot      \\
-// Created By Unlisting \\
+// Created By Flyingandcoding \\
 
 // Settings \\
 
-const BotName = "LOL"; // Bot Name \\
-const Token = "Njg0OTM2NTA0MTc4Mzc2NzM1.XmBW4w.3HGbKVj-QmF-iIOwJ4H9JLpioZQ"; // The token for the bot \\
-const SpamMessage = "LOL"; // message you want to spam \\
-const YourDiscordID = 433376107727683590; // Replace with your discord id! \\
+const BotName = "Botname"; // Bot Name \\
+const Token = "Token"; // The token for the bot \\
+const SpamMessage = "Uh"; // message you want to spam \\
+const YourDiscordID = id; // Replace with your discord id! \\
 
 // Bot \\
 
@@ -16,7 +16,7 @@ const prefix = ";";
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
-	bot.user.setGame("Getting Things Ready", "https://twitch.tv/")
+	bot.user.setGame("UwU", "https://twitch.tv/")
 
 	console.log(`${BotName} Loaded!`);
 
@@ -39,7 +39,7 @@ bot.on("message", async message => {
     	setInterval(function(){
     		Ping.send(SpamMessage)
     	},
-    	  92929292929292929298383818
+    	  NumberofPings
     	);
     };
 });
